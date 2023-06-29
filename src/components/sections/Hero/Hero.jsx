8363440334fa3182aad40b5230467e9a09b9ef01
Maxime Petit <contactmaximepetit@gmail.com>
2023-06-29 14:51:10 +0200

    :sparkles: add content & style of hero

--- src/components/sections/Hero/Hero.jsx
+++ src/components/sections/Hero/Hero.jsx
@@ -1,16 +1,54 @@
+import { useRef, useEffect } from "react";
 import styles from "./Hero.module.scss";
 import { VideoOnScroll } from "../../Video/VideoOnScroll/VideoOnScroll";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import PropTypes from "prop-types";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Hero() {
+  const heroRef = useRef(null);
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: heroRef.current,
+        start: "top+=100px",
+        end: "top+=250px",
+        scrub: true,
+      },
+    });
+
+    tl.fromTo(
+      titleRef.current,
+      { opacity: 1 },
+      {
+        opacity: 0,
+        ease: "power2.out",
+      }
+    );
+  }, []);
+
   return (
-    <section className={styles.hero}>
+    <section ref={heroRef} className={styles.hero}>
+      <div className={styles.hero__content}>
+        <h1 ref={titleRef} className={styles.hero__content__title}>
+          Xiaomi Xiaoai Pro
+        </h1>
+      </div>
       <VideoOnScroll
         className={styles.video}
-        folder="/v1687899930/xiaomi/sequence-01/"
+        folder="/xiaomi/sequence-01/"
         count={90}
         extension="png"
-        pad="2"
+        pad={2}
       />
     </section>
   );
 }
+
+Hero.propTypes = {
+  className: PropTypes.string,
+};
