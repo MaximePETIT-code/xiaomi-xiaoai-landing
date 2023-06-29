f71f1e81853dd6ef181c90a8902d245666360622
Maxime Petit <contactmaximepetit@gmail.com>
2023-06-29 14:50:22 +0200

    :sparkles: add smoothScroll effect and add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import Hero from "./components/sections/Hero/Hero"
+import Hero from "./components/sections/Hero/Hero";
+import Introduction from "./components/sections/Introduction/Introduction";
+import ShowProduct from "./components/sections/ShowProduct/ShowProduct";
+import { Lenis as ReactLenis } from "@studio-freight/react-lenis";
 
 function App() {
+  const options = {
+    lerp: 0.07,
+    smooth: true,
+    direction: "vertical",
+  };
+
   return (
-    <Hero/>
-  )
+    <>
+      <ReactLenis root options={{ ...options }}>
+        <Hero />
+        <Introduction />
+        <ShowProduct />
+      </ReactLenis>
+    </>
+  );
 }
 
-export default App
+export default App;
