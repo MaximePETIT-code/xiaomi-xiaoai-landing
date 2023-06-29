eb77ac7c434d8cab951279f6a0a47f7bd5dee48e
Maxime Petit <contactmaximepetit@gmail.com>
2023-06-29 14:48:50 +0200

    :sparkles: add ShowProduct section

---
+++ src/components/sections/ShowProduct/ShowProduct.jsx
@@ -0,0 +1,56 @@
+import { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import styles from "./ShowProdcut.module.scss";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function ShowProduct() {
+  const glowRef = useRef(null);
+
+  useEffect(() => {
+    const glowImage = glowRef.current;
+
+    gsap.to(glowImage, {
+      scrollTrigger: {
+        trigger: glowImage,
+        start: "top 40%",
+        end: "bottom 10%",
+        scrub: true,
+      },
+      width: "900px",
+    });
+  }, []);
+
+  return (
+    <section className={styles.showProduct}>
+      <div className={styles.showProduct__product}>
+        <img
+          src="https://res.cloudinary.com/dvx9zz0jq/image/upload/w_400,q_auto:good,e_sharpen,f_auto/xiaomi/pictures/product.png"
+          alt="Xiaomi Xiaoai Pro black"
+        />
+
+        <div className={styles.showProduct__infos}>
+          <h3 className={styles.showProduct__subtitle}>
+            Music. Technology. Perfected
+          </h3>
+          <h2 className={styles.showProduct__title}>
+            Immersive Sound Experience
+          </h2>
+          <p className={styles.showProduct__descr}>
+            Immerse yourself in a world of pure sound with our next-generation
+            smart speaker.
+          </p>
+        </div>
+      </div>
+
+      <div className={styles.showProduct__glow}>
+        <img
+          src="https://res.cloudinary.com/dvx9zz0jq/image/upload/xiaomi/assets/glow.png"
+          alt="Xiaomi Xiaoai Pro black"
+          ref={glowRef}
+        />
+      </div>
+    </section>
+  );
+}
