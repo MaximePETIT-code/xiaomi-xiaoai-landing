465b8d3d29db9696c814ff73400b70d236561d8a
Maxime Petit <contactmaximepetit@gmail.com>
2023-07-04 18:10:34 +0200

    :bento: update cloudinary images

--- src/components/sections/Introduction/Introduction.jsx
+++ src/components/sections/Introduction/Introduction.jsx
@@ -7,15 +7,14 @@ export default function Introduction() {
     <section className={styles.introduction}>
       <div className={styles.introduction__container}>
         <AnimatedTitle>
-          Unleash the power of sound with{" "}
-          <span className="orange">Xiaomi Xiaoai Pro</span> - The ultimate
+          Unleash the power of sound with Xiaomi Xiaoai Pro - The ultimate
           speaker that will revolutionize your audio world!
         </AnimatedTitle>
       </div>
       <DoubleImage
         srcs={[
-          "https://res.cloudinary.com/dvx9zz0jq/image/upload/v1687991520/xiaomi/pictures/party.png",
-          "https://res.cloudinary.com/dvx9zz0jq/image/upload/v1687991520/xiaomi/pictures/controls.png",
+          "https://res.cloudinary.com/dvx9zz0jq/image/upload/xiaomi/pictures/packshot.png",
+          "https://res.cloudinary.com/dvx9zz0jq/image/upload/xiaomi/pictures/packshot2.png",
         ]}
       />
     </section>
